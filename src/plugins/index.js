a51b1e393dea2569a53a1bddf4120ea4f563a5be
gloria <pureainu@gmail.com>
2018-10-18 11:44:17 +0900

    [Add] icon package - fontawesome, material-icons

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,29 +1,15 @@
 import Vue from 'vue';
-import Quasar, * as All from 'quasar-framework/dist/quasar.mat.esm';
+import Quasar, * as All from 'quasar-framework/dist/quasar.ios.esm';
 
 import '@/styles/quasar.styl';
 import 'quasar-framework/dist/quasar.ie.polyfills';
 
+// icon
+import 'quasar-extras/fontawesome';
+import 'quasar-extras/material-icons';
+
 Vue.use(Quasar, {
+  config: {},
   components: All,
   directives: All,
 });
-
-// /* javascript library */
-// import Lodash from './lodash';
-// import Moment from './moment';
-// import axios from './axios';
-//
-// /* ui vip components */
-// import VuComponent from 'vip-component';
-// import 'vip-component/dist/style/vu-component.min.css';
-// /* ui custom components */
-// import CustomComponent from '@/components/global';
-//
-// /** Install Plugins **/
-// Vue.prototype.$http = axios;
-// // Vue.use(Element)
-// Vue.use(VuComponent);
-// Vue.use(CustomComponent);
-// Vue.use(Lodash);
-// Vue.use(Moment);
