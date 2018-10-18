6b83e12ab931866a782f337a8e5ad8f6d3f0346b
gloria <pureainu@gmail.com>
2018-10-18 17:57:31 +0900

    [Add] flag plugin

--- src/App.vue
+++ src/App.vue
@@ -10,5 +10,6 @@ export default {
 };
 </script>
 
-<style>
+<style lang="stylus">
+@import './styles/quasar.styl';
 </style>
--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,9 +1,7 @@
 import Vue from 'vue';
 import Quasar, * as All from 'quasar-framework/dist/quasar.ios.esm';
 
-import '@/styles/quasar.styl';
 import 'quasar-framework/dist/quasar.ie.polyfills';
-
 // icon
 import 'quasar-extras/fontawesome';
 import 'quasar-extras/material-icons';
