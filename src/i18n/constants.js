4b0ac73e545a84d9aae8a44bfdba88cb43a921b8
gloria <pureainu@gmail.com>
2018-10-19 13:31:28 +0900

    [Modify] locale

--- src/i18n/constants.js
+++ src/i18n/constants.js
@@ -1,6 +1,10 @@
 export const DEFAULT_LANGUAGE = 'ko';
 export const FALLBACK_LANGUAGE = 'ko';
-export const SUPPORTED_LANGUAGES = [
-  'ko',
-  'en',
-];
+export const SUPPORTED_LANGUAGES = Object.freeze({
+  ko: {
+    label: 'Korean',
+  },
+  en: {
+    label: 'English',
+  },
+});
