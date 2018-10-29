81db960868f505045e124ebcfab44372d10cc25b
gloria <pureainu@gmail.com>
2018-10-29 15:45:10 +0900

    [Modify] routes - for checking auth

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,25 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+
+// import auth from '@/utils/auth';
 import routes from './routes';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: __dirname,
   routes,
 });
+
+// check auth
+router.beforeEach((to, from, next) => {
+  // if (to.matched.some(record => record.meta.requiresAuth) && !auth.getToken()) {
+  //   next({
+  //     name: 'Login',
+  //   });
+  // }
+  next();
+});
+
+export default router;
