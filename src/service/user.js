cc5cb92e19e562785df91a7d78ddfe460a956d04
gloria <pureainu@gmail.com>
2018-10-29 15:12:09 +0900

    [Modify] service sample

--- src/service/user.js
+++ src/service/user.js
@@ -1,55 +1,26 @@
-// import request from '@/utils/request';
-//
-// export function loginByUsername(username, password) {
-//   const data = {
-//     username,
-//     password
-//   }
-//   return request({
-//     url: '/auth/login',
-//     method: 'post',
-//     data: qs.stringify(data)
-//   })
-// }
-//
-// export function logout() {
-//   return request({
-//     url: '/auth/logout',
-//     method: 'post',
-//     loading:"hourglass"
-//   })
-// }
-//
-// export function getUserInfo() {
-//   return request({
-//     url: '/user/info',
-//     method: 'get',
-//     loading:"gears"
-//   })
-// }
+import request from '@/utils/request';
 
-// export function login(username, password) {
-//   return request({
-//     url: '/user/login',
-//     method: 'post',
-//     data: {
-//       username,
-//       password
-//     }
-//   })
-// }
-//
-// export function getInfo(token) {
-//   return request({
-//     url: '/user/info',
-//     method: 'get',
-//     params: { token }
-//   })
-// }
-//
-// export function logout() {
-//   return request({
-//     url: '/user/logout',
-//     method: 'post'
-//   })
-// }
+const service = (() => {
+  const baseURI = '/user';
+
+  return {
+    login(username, password) {
+      return request({
+        url: '/login',
+        method: 'post',
+        data: {
+          username,
+          password,
+        },
+      });
+    },
+
+    logout() {
+      return request({
+        url: `${baseURI}`,
+      });
+    },
+  };
+})();
+
+export default service;
